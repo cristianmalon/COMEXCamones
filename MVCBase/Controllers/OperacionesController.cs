aa407e493604d79fc4392a6012f7f191858f30ad
felipeecc <felipeecc@hotmail.com>
2025-10-02 13:19:56 -0500

    dd

--- MVCBase/Controllers/OperacionesController.cs
+++ MVCBase/Controllers/OperacionesController.cs
@@ -631,7 +631,7 @@ namespace MVCBase.Controllers
 
                     var datos = new Request<Operaciones>();
                     datos.entidad = entidad;
-                    response = new OperacionesAplicacion(new OperacionesRepositorio()).Eliminar(datos);
+                    response = new OperacionesAplicacion(new OperacionesRepositorio()).EliminarE(datos);
 
                     return Json(new
                     {
